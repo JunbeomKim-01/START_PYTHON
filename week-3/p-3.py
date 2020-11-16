7a15533dbe1821e3ea9b3084097b9c1af05b1d18
unknown <estjunbeom@gmail.com>
2020-11-16 15:12:31 +0900

    2020_11_16

--- week-3/p-3.py
+++ week-3/p-3.py
@@ -1,1 +1,20 @@
 # 제어문
+# 조건문 반복문
+
+money = True
+if money:
+    print("True")
+else:
+    print("nope")
+
+a = 1
+b = 2
+if a < b:
+    print("vsv")
+else:
+    print("das")
+
+if False or False:
+    print("hi")
+else:
+    print("hey")
