e085e73b8650b32721667474b0d12fea2463db1f
unknown <estjunbeom@gmail.com>
2020-11-18 17:15:41 +0900

    2020_11_18

--- week-3/p-3.py
+++ week-3/p-3.py
@@ -45,4 +45,58 @@ def plus_(a, b):
     return a+b
 
 
-print(plus_(3, 2))
+print(plus_(a=3, b=2))
+
+
+def say_HEllo(name, age):
+    return "Hello" + name + "you are" + age+"years old"
+    # f"hell {name} you are {age} years old"
+
+
+hello = say_HEllo(name="kim", age=20)  # 순서 상관X 이름이 중요
+print(hello)
+
+# make No.7 function Challenge! + - / * %
+
+
+def plus(a=0, b=0):
+    return float(a) + float(b)
+
+
+def minus(a=0, b=0):
+    ma = float(a)
+    mb = float(b)
+    print(ma, mb)
+    print(type(ma))
+    return ma - mb
+
+
+def times(a=0, b=0):
+    return float(a) * float(b)
+
+
+def division(a=0, b=0):
+    return float(a) / float(b)
+
+
+def reminder(a=0, b=0):
+    return float(a) % float(b)
+
+
+def nigate(a=0):
+    return -float(a)
+
+
+def power(a=0, b=0):
+    return float(a) ** float(b)
+
+
+p_plus = plus("2", 3)
+m_minus = minus(2, "3")
+t_times = times("2", 3)
+d_division = division(2, "3")
+r_reminder = reminder("2", 3)
+n_nigate = nigate("2")
+p_power = power(2, "3")
+
+print(p_plus, m_minus, t_times, d_division, r_reminder, n_nigate, p_power)
